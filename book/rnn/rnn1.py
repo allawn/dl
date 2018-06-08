f33939906b58ecd6b70b80c02f55078e67a84e8b
xawwei <xawwei@cn.ibm.com>
2018-06-08 09:53:23 +0800

    rnn

--- book/rnn/rnn1.py
+++ book/rnn/rnn1.py
@@ -9,9 +9,10 @@ for i in range(rnnLayerNum):
 
 multiRnnCell=tf.nn.rnn_cell.MultiRNNCell(rnnCells)
 timesteps=5;
-input=tf.placeholder(tf.float32, [1, timesteps, 1], name='input_x')
-sequence_length=[2]
-initial_state=multiRnnCell.zero_state(batch_size=1,dtype=tf.float32)
+batch_size=2
+input=tf.placeholder(tf.float32, [batch_size, timesteps, 1], name='input_x')
+sequence_length=[2,5]
+initial_state=multiRnnCell.zero_state(batch_size=batch_size,dtype=tf.float32)
 
 outputs, final_state =tf.nn.dynamic_rnn(multiRnnCell,input,sequence_length=sequence_length,initial_state=initial_state,dtype=tf.float32)
 print(outputs)
@@ -22,6 +23,6 @@ initopt=tf.initialize_all_variables()
 
 with tf.Session() as sess:
     sess.run(initopt)
-    inputVal=np.ones([1,5,1],dtype=np.float32)
+    inputVal=np.ones([2,5,1],dtype=np.float32)
     print(sess.run(outputsList[1], feed_dict={input:inputVal}))
     print(sess.run(final_state, feed_dict={input:inputVal}))
